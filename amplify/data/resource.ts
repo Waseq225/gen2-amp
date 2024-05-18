a2b9abbe2211b4ee33ffc39365448da02343816d
Waseq225 <mohammadwaseq225@gmail.com>
2024-05-18 14:54:58 +1000

    field level auth

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -17,7 +17,7 @@ const schema = a.schema({
       eventId: a.id(),
       content: a.string(),
       availableTickets: a.integer(),
-      carts: a.hasMany("Cart", "eventId")
+      carts: a.hasMany("Cart", "eventId").authorization(allow => [allow.owner()])
     })
     .authorization((allow) => [allow.publicApiKey()]),
   Cart: a
